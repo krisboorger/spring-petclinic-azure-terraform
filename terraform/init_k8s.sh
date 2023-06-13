93a981dca6d4696a2b2e0c3e2d3a932658f6d0de
krisboorger <krisboorger@gmail.com>
2023-06-13 19:59:07 +0200

    added the container launching to init_k8s.sh

--- terraform/init_k8s.sh
+++ terraform/init_k8s.sh
@@ -3,3 +3,6 @@
 az aks get-credentials --name k8s --resource-group petclinic-k8s
 kubectl apply -f k8s/init-namespace/
 kubectl apply -f k8s/init-services/
+
+export REPOSITORY_PREFIX=springcommunity
+./scripts/deployToKubernetes.sh
